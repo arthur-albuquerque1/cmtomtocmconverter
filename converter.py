61b7d06161c4e12f760cb4d4f0b3d3a530a1d36e
Arthur Albuquerque <80676157+arthur-albuquerque1@users.noreply.github.com>
2023-01-24 14:13:33 -0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -11,23 +11,20 @@ def conversor():
 
             if opt == 1:
                 meters = float(input("Digite o valor em metros: ")) * 100
-                text_result1 = " centímetros"
-                print(f"{meters}{text_result1}")
+                print(f'{meters} centímetros')
                 
             if opt == 2:
                 centimeters = float(input("Digite o valor em centímetros: ")) / 100
-                text_result2 = " metros"
-                print(f"{centimeters}{text_result2}")
+                print(f'{centimeters} metros')
 
             opt_message = int(input('Se deseja fazer outra operação digite 1, se deseja sair digite 2: ')) 
             if opt_message == 2:
                  exit = True
             elif 0 <= opt_message > 2:
                  print("Opção inválida, tente novamente")
-            
-            if opt_message == 1:
+            elif opt_message == 1:
                 conversor()
-            if opt_message == 2:
+            elif opt_message == 2:
                 exit = True
                 print("Até a próxima!")
 
