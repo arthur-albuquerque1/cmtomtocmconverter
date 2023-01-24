7031c48b474409b017cb6d68010b2c670f27c55d
Arthur Albuquerque <80676157+arthur-albuquerque1@users.noreply.github.com>
2023-01-24 14:21:02 -0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -2,20 +2,24 @@ def conversor():
         exit = False
         while not exit:
             print("Seja Bem vindo ao nosso conversor de centímetros para metros, e vice-versa")
-            print("1. Metros para Centímetros")
-            print("2. Centímetros para Metros")
+            print("1. Metros para centímetros")
+            print("2. Centímetros para metros")
+            print("3. Metros para milímetros")
 
             opt = int(input("Selecione a opção desejada: "))
-            if 0 <= opt > 2:
+            if 0 <= opt > 3:
                  print("Opção inválida, tente novamente")
 
-            if opt == 1:
+            elif opt == 1:
                 meters = float(input("Digite o valor em metros: ")) * 100
                 print(f'{meters} centímetros')
                 
-            if opt == 2:
+            elif opt == 2:
                 centimeters = float(input("Digite o valor em centímetros: ")) / 100
                 print(f'{centimeters} metros')
+            elif opt == 3:
+                meters = float(input("Digite o valor em metros: ")) * 1000
+                print(f'{meters} milímetros')
 
             opt_message = int(input('Se deseja fazer outra operação digite 1, se deseja sair digite 2: ')) 
 
